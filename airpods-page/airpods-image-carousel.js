508b8238dedefa465c3c5fb39d87a1f306949b9a
Roderick002 <ballaranroderick@gmail.com>
2023-11-04 16:00:14 +0800

    Finalize content

--- airpods-page/airpods-image-carousel.js
+++ airpods-page/airpods-image-carousel.js
@@ -5,9 +5,13 @@ const nextButton = document.getElementById("next");
 let translateX = 0;
 const step = 0.14;
 
+var click = 0;
+
 prevButton.addEventListener("click", function () {
     if (translateX < 0) {
         translateX += step;
+
+        click --;
         updateImageContainer();
     }
 });
@@ -16,10 +20,24 @@ nextButton.addEventListener("click", function () {
     const maxTranslateX = (imageContainer.children.length - 2) * -step;
     if (translateX > maxTranslateX) {
         translateX -= step;
+
+        click ++;
         updateImageContainer();
     }
 });
 
 function updateImageContainer() {
     imageContainer.style.transform = `translateX(${translateX * 100}%)`;
+    if (click == 0) {
+        prevButton.disabled = true;
+    }else{
+        prevButton.disabled = false;
+    }
+
+    if (click == 3) {
+        nextButton.disabled = true;
+    }else{
+        nextButton.disabled = false;
+    }
+
 }
